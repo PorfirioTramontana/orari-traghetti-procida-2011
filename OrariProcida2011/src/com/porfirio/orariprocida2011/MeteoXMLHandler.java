c4f934b640cbc5028be5baec19abbff71d59c0cb
porfirio.tramontana@gmail.com <porfirio.tramontana@gmail.com@e62c656e-40d2-95fa-17bb-987da87612dd>
2011-11-22 14:35:15 +0000

    Modifiche nella conversione kmh --> Beaufort. Ora il valore preciso Beaufort si ha nel punto medio dell'intervallo anzichè sul minimo.

--- OrariProcida2011/src/com/porfirio/orariprocida2011/MeteoXMLHandler.java
+++ OrariProcida2011/src/com/porfirio/orariprocida2011/MeteoXMLHandler.java
@@ -18,18 +18,18 @@ public class MeteoXMLHandler extends DefaultHandler{
 
 		@Override
 	    public void startDocument() throws SAXException {
-	        Log.d("ORARI","Start document");
+//	        Log.d("ORARI","Start document");
 	    } 
 	    
 	    @Override
 	    public void endDocument() throws SAXException {
-	    	Log.d("ORARI","End document");
+//	    	Log.d("ORARI","End document");
 	    } 
 	    
 	    @Override
 	    public void startElement(String uri, String localName, String qName,
 	    Attributes attributes) throws SAXException {
-	    	Log.d("ORARI","Start element");
+//	    	Log.d("ORARI","Start element");
 	    	if (localName.equals("wind_condition")) {
 	    		/** Get attribute value */
 	    		String attr = attributes.getValue("data");
@@ -57,6 +57,8 @@ public class MeteoXMLHandler extends DefaultHandler{
 	    	else if (attr.contains(" NW "))
 	    		callingActivity.meteo.setWindDirection(315);
 			
+	    	Log.d("ORARI","Vento da "+callingActivity.meteo.getWindDirection());	    	
+	    	
 	    	//TODO gestire anche i kmh
 	    	int pos= attr.indexOf(" at ");
 	    	int pos2=attr.indexOf("mph");
@@ -65,35 +67,36 @@ public class MeteoXMLHandler extends DefaultHandler{
 	    	if (wkmh<=1)
 	    		callingActivity.meteo.setWindBeaufort(0.0);
 	    	else if (wkmh>1 && wkmh<=5)
-	    		callingActivity.meteo.setWindBeaufort(1+(wkmh-1)/(5-1));
+	    		callingActivity.meteo.setWindBeaufort(1+(wkmh-3)/(5-1));
 	    	else if (wkmh>=6 && wkmh<=11)
-	    		callingActivity.meteo.setWindBeaufort(2+(wkmh-6)/(11-6));
+	    		callingActivity.meteo.setWindBeaufort(2+(wkmh-8.5)/(11-6));
 	    	else if (wkmh>=12 && wkmh<=19)
-	    		callingActivity.meteo.setWindBeaufort(3+(wkmh-12)/(19-12));
+	    		callingActivity.meteo.setWindBeaufort(3+(wkmh-15.5)/(19-12));
 	    	else if (wkmh>=20 && wkmh<=28)
-	    		callingActivity.meteo.setWindBeaufort(4+(wkmh-20)/(28-20));
+	    		callingActivity.meteo.setWindBeaufort(4+(wkmh-24)/(28-20));
 	    	else if (wkmh>=29 && wkmh<=38)
-	    		callingActivity.meteo.setWindBeaufort(5+(wkmh-29)/(38-29));
+	    		callingActivity.meteo.setWindBeaufort(5+(wkmh-33.5)/(38-29));
 	    	else if (wkmh>=39 && wkmh<=49)
-	    		callingActivity.meteo.setWindBeaufort(6+(wkmh-39)/(49-39));
+	    		callingActivity.meteo.setWindBeaufort(6+(wkmh-44)/(49-39));
 	    	else if (wkmh>=50 && wkmh<=61)
-	    		callingActivity.meteo.setWindBeaufort(7+(wkmh-50)/(61-50));
+	    		callingActivity.meteo.setWindBeaufort(7+(wkmh-55.5)/(61-50));
 	    	else if (wkmh>=62 && wkmh<=74)
-	    		callingActivity.meteo.setWindBeaufort(8+(wkmh-62)/(74-62));
+	    		callingActivity.meteo.setWindBeaufort(8+(wkmh-68)/(74-62));
 	    	else if (wkmh>=75 && wkmh<=88)
-	    		callingActivity.meteo.setWindBeaufort(9+(wkmh-75)/(88-75));
+	    		callingActivity.meteo.setWindBeaufort(9+(wkmh-81.5)/(88-75));
 	    	else if (wkmh>=89 && wkmh<=102)
-	    		callingActivity.meteo.setWindBeaufort(10+(wkmh-89)/(102-89));
+	    		callingActivity.meteo.setWindBeaufort(10+(wkmh-95.5)/(102-89));
 	    	else if (wkmh>=103 && wkmh<=117)
-	    		callingActivity.meteo.setWindBeaufort(11+(wkmh-103)/(117-103));
+	    		callingActivity.meteo.setWindBeaufort(11+(wkmh-110)/(117-103));
 	    	else if (wkmh>=118)
 	    		callingActivity.meteo.setWindBeaufort(12);
+	    	Log.d("ORARI","Vento forza "+callingActivity.meteo.getWindBeaufort());
 		}
 
 		@Override
 	    public void endElement(String uri, String localName, String qName)
 	    throws SAXException {
-	    	Log.d("ORARI","End element");
+//	    	Log.d("ORARI","End element");
 	    }
 
 //	    /** Called to get tag characters ( ex:- <name>AndroidPeople</name>
